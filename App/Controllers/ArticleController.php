8a1542ffb09c50e4a09a0fb1dcee9de6b3891c07
BalthazzrCorr <peev.petar@gmail.com>
2016-12-15 19:56:07 +0200

    Article Controller and Stuff

---
+++ App/Controllers/ArticleController.php
@@ -0,0 +1,82 @@
+<?php 
+
+use Yee\Managers\Controller\Controller;
+use Yee\Managers\CacheManager;
+use App\Models\ArticleModel;
+use App\Models\CategoryModel;
+
+class ArticleController extends Controller {
+
+    /**
+     * @Route('/article')
+     * @Name('article.index')
+     */
+    public function indexAction() {
+
+        /** @var Yee\Yee $yee */
+        $app = $this->getYee();
+
+        if (isset($_SESSION['isLogged']) === true) {
+
+            
+            $categoryModel = new CategoryModel();
+
+            $categorys = $categoryModel->getCategory();
+
+            $javascript = array(
+                '/js/addArticle.js',
+                );
+
+
+
+            $data = array(
+                'title'             => 'Add Article',
+                'javascript'        => $javascript,
+                'categoryDetails'   => $categorys,
+                );
+
+            $app->render('addArticle.twig', $data);
+     
+        } else {
+
+            $app->redirect('/login');
+        }
+    }
+
+
+
+    /**
+     * @Route('/articleList')
+     * @Name('article.index')
+     */
+    public function listArticles() {
+
+        /** @var Yee\Yee $yee */
+        $app = $this->getYee();
+
+
+        if ($_SESSION['isAdmin'] != 1) {
+        
+            $app->redirect("/account");
+        }
+
+
+        $article = new ArticleModel();
+
+        // order by date ASC
+        $commentsList = $article->getComments();
+
+        // order by date DESC
+        $commList = array_reverse($commentsList);
+
+        // var_dump();
+        // die;
+
+        $data = array(
+                'title' => 'List of Articles',
+                'commentDetails' => $commList,
+                );
+
+        $app->render('articleList.twig',$data);
+    }
+}
---
+++ App/Controllers/CategoryController.php
@@ -0,0 +1,144 @@
+<?php
+
+use Yee\Managers\Controller\Controller;
+use Yee\Managers\CacheManager;
+use App\Models\CategoryModel;
+
+// Category Controller for Admin...
+
+class CategoryController extends Controller {  
+     /**
+     * @Route('/category')
+     * @Name('categoryAdd.index')
+     */
+    public function indexAction( )
+    {
+      
+        /** @var Yee\Yee $yee */
+        $app = $this->getYee();
+
+        $javascript = array(
+
+            '/js/categoryAdd.js',
+          );
+        
+        $data = array(
+
+            'title' => 'New Category',
+            'javascript' => $javascript,
+          );
+
+        if ( !isset($_SESSION['isLogged']) ) {
+
+            $app->redirect('/login');   
+        }
+
+        if ($_SESSION['isAdmin']) {
+          $app->render('/categoryAdd.twig',$data);
+
+        } else {
+
+          $app->redirect('/account'); 
+
+        }
+
+    }
+     /**
+     * @Route('/categoryUpdate/:id') 
+     * @Name('updateCategory.index')
+     */
+     public function updateCategory($id) {
+
+          $app = $this->getYee();
+          
+          $categoryId = $id;
+
+          if ($_SESSION['isAdmin'] != 1) {
+            $app->redirect('/account');
+          }
+
+          $categoryModel = new CategoryModel();
+          $categoryProperty = $categoryModel->getCategoryById($categoryId);
+
+          if ($categoryProperty == null) {
+              
+              $app->redirect('/categoryList');
+          }
+
+
+          $javascript = array(
+
+            '/js/categoryUpdate.js',
+            );
+
+
+          $data = array(
+            'title' => 'Update Category',
+            'javascript' => $javascript,
+            'categoryId' => $categoryId,
+            'categoryName' => $categoryProperty['name'],
+            ); 
+          $app->render('/categoryUpdate.tpl',$data);
+     }
+
+     /**
+     * @Route('/categoryDelete/:id') 
+     * @Name('updateCategory.index')
+     */
+     public function deleteCategory($id) {
+
+          $app = $this->getYee();
+
+          if ($_SESSION['isAdmin'] != 1) {
+            $app->redirect('/myProject/account');
+          }
+
+          $categoryModel = new CategoryModel();
+          $categoryProperty = $categoryModel->getCategoryById($id);
+
+          if ($categoryProperty == null) {
+              
+              $app->redirect('/categoryList');
+          }
+
+          $javascript = array(
+
+            '/js/categoryDelete.js',
+            );
+
+          $data = array(
+            'title' => 'Delete Category',
+            'javascript' => $javascript,
+            'categoryId' => $categoryProperty['id'],      
+            'categoryName' => $categoryProperty['name'],
+            ); 
+          $app->render('/pages/categoryDelete.tpl',$data);
+     }
+
+     /**
+     * @Route('/categoryList') 
+     * @Name('updateCategory.index')
+     */
+     public function listCategory() {
+
+            $app = $this->getYee();
+
+            $categoryModel = new CategoryModel();
+            $categoryList = $categoryModel->getCategory();
+
+            if (!isset($_SESSION['isLogged']) ) {
+                $app->redirect('/login');
+            }
+
+            if ($_SESSION['isAdmin'] != 1) {
+                $app->redirect('/account');
+            }
+
+            $data = array(
+                'title' => 'List Category',
+                'categoryDetails' => $categoryList,
+            ); 
+
+            $app->render('/pages/categoryList.tpl',$data);
+     }
+}
